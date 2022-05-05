9c9e6eaa426864a25cc16ceb30321352ce4f084a
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-05-04 20:48:27 -0700

    🤦

--- aws.js
+++ aws.js
@@ -1,6 +1,6 @@
 const credentials = {
-  "accessKeyId": "***REMOVED***",
-  "secretAccessKey": "***REMOVED***",
+  "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+  "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
 };
 const {
   TranscribeStreamingClient,
